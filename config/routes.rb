ffbec3a4d6f195a1d861022b0e982b55279685fd
Kevin VanderLugt <kevin.vanderlugt@gmail.com>
2013-09-21 19:52:55 -0700

    Add tasks list scaffolds, no json format

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
-Todos::Application.routes.draw do
+Todos::Application.routes.draw do  
+  resources :tasks
+  root 'tasks#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
